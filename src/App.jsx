41e4f0426f6591bb727dbf8e53c35ce885144e9b
Natalie Gillam <ngillam@hotmail.co.uk>
2023-08-27 13:22:05 +0100

    created dashboard component and loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Routes 
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard/>,
+    loader: dashboardLoader
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
 function App() {
   return <div className="App">
-    <h1>hi</h1>
+    <RouterProvider router={router} />
   </div>;
 }
 
