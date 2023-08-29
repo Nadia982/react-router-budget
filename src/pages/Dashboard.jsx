9b2df0d63c517e71562f8418fd16ae82db9fe4fe
Natalie Gillam <ngillam@hotmail.co.uk>
2023-08-29 23:47:27 +0100

    added <Table> component to list out expenses

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ import Intro from "../components/Intro";
 import AddBudgetForm from "./../components/AddBudgetForm";
 import AddExpenseForm from './../components/AddExpenseForm';
 import BudgetItem from './../components/BudgetItem';
+import Table from './../components/Table';
 
 //library imports
 import { toast } from "react-toastify";
@@ -17,7 +18,8 @@ import { toast } from "react-toastify";
 export function dashboardLoader() {
   const userName = fetchData("userName");
   const budgets = fetchData("budgets");
-  return { userName, budgets };
+  const expenses = fetchData("expenses");
+  return { userName, budgets, expenses };
 }
 
 //action
@@ -66,7 +68,7 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName, budgets } = useLoaderData();
+  const { userName, budgets, expenses } = useLoaderData();
   return (
     <>
       {userName ? (
@@ -89,6 +91,15 @@ const Dashboard = () => {
                     ))
                   }
                 </div>
+                  {
+                    expenses && expenses.length > 0 && (
+                      <div className="grid-md">
+                        <h2>Recent Expenses</h2>
+                        <Table expenses = {expenses.sort((a, b) => b.createdAt - a.createdAt)} />
+                      </div>
+                    )
+
+                  }
               </div>
             ) : (
               <div className="grid-sm">
